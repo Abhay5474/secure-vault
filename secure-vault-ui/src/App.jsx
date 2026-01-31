da3876ddc64e6d97d48fe36cfe702b372d132da0
Abhay5474 <125486846+Abhay5474@users.noreply.github.com>
2026-01-31 19:02:42 +0530

    hash routing

--- secure-vault-ui/src/App.jsx
+++ secure-vault-ui/src/App.jsx
@@ -1,25 +1,28 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'; // 1. Import HashRouter
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
-import DecryptPage from './pages/DecryptPage'; // <--- Import this
+import DecryptPage from './pages/DecryptPage';
 import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
 
 function App() {
   return (
-    <Router>
+    // 2. Use HashRouter instead of Router/BrowserRouter
+    <HashRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/decrypt" element={<DecryptPage />} /> {/* <--- New Route */}
-        {/* NEW ROUTES */}
+        <Route path="/decrypt" element={<DecryptPage />} />
+        
+        {/* Password Reset Routes */}
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
+        
         <Route path="/" element={<Navigate to="/login" />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
